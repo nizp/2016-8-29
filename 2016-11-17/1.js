bffc85bbb419b3c3222e552f6f4699835325af0d
nizp <nzp890524@163.com>
2016-11-17 15:57:23 +0800

    down

--- 2016-11-17/1.js
+++ 2016-11-17/1.js
@@ -1,3 +1,8 @@
 function Drag(id){
-	
+	var box = document.getElementById(id);
+	box.onmousedown = fnDown;
+	function(ev){
+		var disX = ev.pageX - box.offsetLeft;
+		var disY = ev.pageY - box.offsetTop;
+	}
 }

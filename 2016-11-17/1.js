81f840bddddc405a8037a5194263900e846dfd7f
ydg <42333636@qq.com>
2017-03-22 11:40:31 +0800

    change 1.js

--- 2016-11-17/1.js
+++ 2016-11-17/1.js
@@ -1,12 +1,12 @@
 function Drag(id){
 	var box = document.getElementById(id);
 	box.onmousedown = fnDown;
-	function fnDown(ev){
-		var disX = ev.pageX - box.offsetLeft;
-		var disY = ev.pageY - box.offsetTop;
+	function fnDown(e){
+		var disX = e.pageX - box.offsetLeft;
+		var disY = e.pageY - box.offsetTop;
 	}
-	function move(ev){
-		box.style.left = ev.pageX - disX + 'px';
-		box.style.top = ev.pageY - disY + 'px';
+	function move(e){
+		box.style.left = e.pageX - disX + 'px';
+		box.style.top = e.pageY - disY + 'px';
 	}
 }
